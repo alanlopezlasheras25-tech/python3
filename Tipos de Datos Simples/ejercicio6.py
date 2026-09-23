8c91cdd123173b1bc5ee4844a96f5819e9f5e138
Alan López <alanlopezlasheras25@gmail.com>
2026-09-23 14:04:16 +0200

    ejercicio 6

---
+++ Tipos de Datos Simples/ejercicio6.py
@@ -0,0 +1,3 @@
+#Escribir un programa que lea un entero positivo, , introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n. La suma de los primeros enteros positivos puede ser calculada de la siguiente forma:
+numero= int(input("Dime un numero entero "))
+print((int(numero) * (int(numero) + 1))/2)
